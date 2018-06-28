269b7f08a5c72639fbc80593a17109495b43e10b
ikalentii <alexey.kalentyev@gmail.com>
2018-06-28 11:09:15 +0700

    Удалил ненужные файлы

--- Model/Events/ValueChangedEventHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Model.Events
-{
-    /// <summary>
-    ///     Делегат изменений значений на номиналах
-    /// </summary>
-    /// <param name="sender">Отправитель</param>
-    public delegate void ValueChangedEventHandler(object sender, ElementValueArgs arguments);
-}
