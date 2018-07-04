4d26c565b46485205d0263877f470f1fdd424c84
YulyaNovichkova <wwwyulya7@gmail.com>
2018-07-04 21:30:53 +0700

    Переименованы названия элементов формы.

--- View/MainForm.cs
+++ View/MainForm.cs
@@ -7,14 +7,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Numerics;
 
 namespace View
 {
+    /// <summary>
+    /// Форма отображения цепи.
+    /// </summary>
     public partial class MainForm : Form
     {
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ToolStripMenuItemChain1.Selected == true)
+            {
+
+            }
+            if (ToolStripMenuItemChain2.Selected == true)
+            {
+
+            }
+            if (ToolStripMenuItemChain3.Selected == true)
+            {
+
+            }
+            if (ToolStripMenuItemChain4.Selected == true)
+            {
+
+            }
+            if (ToolStripMenuItemChain5.Selected == true)
+            {
+
+            }
         }
     }
 }
