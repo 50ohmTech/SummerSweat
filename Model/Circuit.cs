87cd8ed4a843b240f55df0a94a83c70a00fb9aa6
50rubasov <bigsimol666@gmail.com>
2018-09-15 18:44:37 +0700

    Реализована библиотека + интерфейс нод и класс цепи

---
+++ Model/Circuit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using Model.Elements;
+using Model.Elements.Enums;
+
+namespace Model
+{
+    /// <summary>
+    ///     Цепь.
+    /// </summary>
+    public class Circuit
+    {
+
+    }
+}
---
+++ Model/Elements/Resistor.cs
@@ -0,0 +1,43 @@
+﻿#region
+
+using System;
+using System.Numerics;
+
+#endregion
+
+namespace Model.Elements
+{
+    /// <summary>
+    ///     Резистор
+    /// </summary>
+    public class Resistor : ElementBase
+    {
+        #region Public methods
+
+        /// <summary>
+        ///     Конструктор класса Resistor
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="value">Номинал</param>
+        public Resistor(string name, double value) : base(name, value)
+        {
+        }
+
+        /// <summary>
+        ///     Расчет импеданса
+        /// </summary>
+        /// <param name="frequency">Частота сигнала</param>
+        /// <returns>Комплекное сопротивление</returns>
+        public override Complex CalculateZ(double frequency)
+        {
+            if (frequency <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Частота не может быть меньше нуля");
+            }
+
+            return new Complex(Value, 0);
+        }
+
+        #endregion
+    }
+}
