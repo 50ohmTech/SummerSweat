6a5eecfbd1fb94c763876a752964ad44b89689df
chelovecheki <36590480+chelovecheki@users.noreply.github.com>
2018-09-15 21:50:54 +0700

    Сделана базовая реализация

---
+++ Alexander/CircuitCalculator/Model/Circuit.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using Model;
+
+namespace Model
+{
+    /// <summary>
+    ///     Цепь.
+    /// </summary>
+    public class Circuit
+    {
+        #region Fields
+
+        #region Private fields
+
+        /// <summary>
+        ///     Корень цепи.
+        /// </summary>
+        private INode _root;
+
+        #endregion
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        ///     Расчитать импедансы цепи для списка частот.
+        /// </summary>
+        /// <param name="frequencies">Список частот сигнала.</param>
+        /// <returns>Список импедансов цепи.</returns>
+        public List<Complex> CalculateZ(double[] frequencies)
+        {
+            if (frequencies == null)
+            {
+                throw new ArgumentNullException(nameof(frequencies));
+            }
+
+            var impedances = new List<Complex>();
+
+            foreach (var frequency in frequencies)
+            {
+                impedances.Add(_root.CalculateZ(frequency));
+            }
+
+            return impedances;
+        }
+
+        #endregion
+    }
+}
