3d2333b208e6e3237e250fd05690cd36d7a07a9b
injecti0N <injecti0n.kg@gmail.com>
2018-09-17 00:05:01 +0700

    Базовая реализация(неполная)

--- Azizkhan/Circuit/CircuitLibrary/Properties/AssemblyInfo.cs
+++ Azizkhan/Circuit/CircuitLibrary/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("MainForm")]
+[assembly: AssemblyTitle("CircuitLibrary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MainForm")]
+[assembly: AssemblyProduct("CircuitLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("bae7e8dd-7cbd-44d1-abab-623ebd52eff9")]
+[assembly: Guid("64a12286-4b78-42e7-8929-4e6664cb5d43")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
--- Azizkhan/Circuit/MainForm/CircuitMainForm.cs
+++ Azizkhan/Circuit/MainForm/CircuitMainForm.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace MainForm
 {
     public partial class MainForm : Form
     {
+        #region Constructor
+
         public MainForm()
         {
             InitializeComponent();
         }
+
+        #endregion
     }
 }
