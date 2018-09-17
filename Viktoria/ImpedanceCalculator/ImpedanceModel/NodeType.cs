2190d1c2371254d1254094d56f89d75b4e5cfc11
lKrausz <krausz.st@gmail.com>
2018-09-18 00:07:31 +0700

    Реализация библиотеки.

---
+++ Viktoria/ImpedanceCalculator/ImpedanceModel/NodeType.cs
@@ -0,0 +1,14 @@
+﻿namespace ImpedanceModel
+{
+    /// <summary>
+    /// Типы элементов и соединений, которые могут быть в Node
+    /// </summary>
+    public enum NodeType
+    {
+        Paralel,
+        Serial,
+        Resistor,
+        Inductor,
+        Capacitor
+    }
+}
