ec55e0a85c6ad44e3b59b809d20d91c4dc68dbea
nachyn <nachyn@outlook.com>
2018-09-18 00:20:15 +0700

    Добавил родителей.

--- Nachyn/Model/ViewHelpers/InitialCircuitInitializer.cs
+++ Nachyn/Model/ViewHelpers/InitialCircuitInitializer.cs
@@ -35,12 +35,18 @@ namespace Model.ViewHelpers
 
                     SeriesSubcircuit seriesSubcircuit1 = new SeriesSubcircuit();
                     seriesSubcircuit1.Nodes.Add(inductor1);
+                    inductor1.Parent = seriesSubcircuit1;
                     seriesSubcircuit1.Nodes.Add(resistor2);
+                    resistor2.Parent = seriesSubcircuit1;
+
 
                     ParallelSubcircuit parallelSubcircuit1 = new ParallelSubcircuit();
                     seriesSubcircuit1.Nodes.Add(parallelSubcircuit1);
+                    parallelSubcircuit1.Parent = seriesSubcircuit1;
                     parallelSubcircuit1.Nodes.Add(resistor3);
+                    resistor3.Parent = parallelSubcircuit1;
                     parallelSubcircuit1.Nodes.Add(inductor4);
+                    inductor4.Parent = parallelSubcircuit1;
 
                     circuit.AddAfter(null, seriesSubcircuit1);
                     break;
@@ -55,17 +61,23 @@ namespace Model.ViewHelpers
 
                     ParallelSubcircuit parallelSubcircuit1 = new ParallelSubcircuit();
                     parallelSubcircuit1.Nodes.Add(capacitor1);
+                    capacitor1.Parent = parallelSubcircuit1;
 
                     SeriesSubcircuit seriesSubcircuit1 = new SeriesSubcircuit();
                     parallelSubcircuit1.Nodes.Add(seriesSubcircuit1);
+                    seriesSubcircuit1.Parent = parallelSubcircuit1;
 
                     seriesSubcircuit1.Nodes.Add(inductor2);
+                    inductor2.Parent = seriesSubcircuit1;
 
                     ParallelSubcircuit parallelSubcircuit2 = new ParallelSubcircuit();
                     parallelSubcircuit2.Nodes.Add(resistor3);
+                    resistor3.Parent = parallelSubcircuit2;
                     parallelSubcircuit2.Nodes.Add(inductor4);
+                    inductor4.Parent = parallelSubcircuit2;
 
                     seriesSubcircuit1.Nodes.Add(parallelSubcircuit2);
+                    parallelSubcircuit2.Parent = seriesSubcircuit1;
                     circuit.AddAfter(null, parallelSubcircuit1);
 
                     break;
@@ -88,21 +100,32 @@ namespace Model.ViewHelpers
                     SeriesSubcircuit seriesSubcircuit2 = new SeriesSubcircuit();
 
                     parallelSubcircuit1.Nodes.Add(seriesSubcircuit1);
+                    seriesSubcircuit1.Parent = parallelSubcircuit1;
                     parallelSubcircuit1.Nodes.Add(seriesSubcircuit2);
+                    seriesSubcircuit2.Parent = parallelSubcircuit1;
 
                     ParallelSubcircuit parallelSubcircuit2 = new ParallelSubcircuit();
                     ParallelSubcircuit parallelSubcircuit3 = new ParallelSubcircuit();
 
                     seriesSubcircuit1.Nodes.Add(inductor3);
+                    inductor3.Parent = seriesSubcircuit1;
                     seriesSubcircuit1.Nodes.Add(parallelSubcircuit2);
+                    parallelSubcircuit2.Parent = seriesSubcircuit1;
                     parallelSubcircuit2.Nodes.Add(resistor1);
+                    resistor1.Parent = parallelSubcircuit2;
                     parallelSubcircuit2.Nodes.Add(inductor4);
+                    inductor4.Parent = parallelSubcircuit2;
 
                     seriesSubcircuit2.Nodes.Add(inductor2);
+                    inductor2.Parent = seriesSubcircuit2;
                     seriesSubcircuit2.Nodes.Add(resistor5);
+                    resistor5.Parent = seriesSubcircuit2;
                     seriesSubcircuit2.Nodes.Add(parallelSubcircuit3);
+                    parallelSubcircuit3.Parent = seriesSubcircuit2;
                     parallelSubcircuit3.Nodes.Add(resistor6);
+                    resistor6.Parent = parallelSubcircuit3;
                     parallelSubcircuit3.Nodes.Add(inductor7);
+                    inductor7.Parent = parallelSubcircuit3;
                     circuit.AddAfter(null, parallelSubcircuit1);
                     break;
                 }
@@ -122,13 +145,21 @@ namespace Model.ViewHelpers
                     ParallelSubcircuit parallelSubcircuit2 = new ParallelSubcircuit();
 
                     seriesSubcircuit1.Nodes.Add(parallelSubcircuit1);
+                    parallelSubcircuit1.Parent = seriesSubcircuit1;
                     seriesSubcircuit1.Nodes.Add(parallelSubcircuit2);
+                    parallelSubcircuit2.Parent = seriesSubcircuit1;
                     seriesSubcircuit1.Nodes.Add(resistor3);
+                    resistor3.Parent = seriesSubcircuit1;
                     parallelSubcircuit1.Nodes.Add(resistor1);
+                    resistor1.Parent = parallelSubcircuit1;
                     parallelSubcircuit1.Nodes.Add(inductor4);
+                    inductor4.Parent = parallelSubcircuit1;
                     parallelSubcircuit2.Nodes.Add(capacitor2);
+                    capacitor2.Parent = parallelSubcircuit2;
                     parallelSubcircuit2.Nodes.Add(resistor5);
+                    resistor5.Parent = parallelSubcircuit2;
                     parallelSubcircuit2.Nodes.Add(inductor6);
+                    inductor6.Parent = parallelSubcircuit2;
                     circuit.AddAfter(null, seriesSubcircuit1);
 
                     break;
@@ -152,18 +183,30 @@ namespace Model.ViewHelpers
                     ParallelSubcircuit parallelSubcircuit2 = new ParallelSubcircuit();
                     ParallelSubcircuit parallelSubcircuit3 = new ParallelSubcircuit();
                     seriesSubcircuit1.Nodes.Add(parallelSubcircuit1);
+                    parallelSubcircuit1.Parent = seriesSubcircuit1;
                     parallelSubcircuit1.Nodes.Add(resistor1);
+                    resistor1.Parent = parallelSubcircuit1;
                     parallelSubcircuit1.Nodes.Add(inductor4);
+                    inductor4.Parent = parallelSubcircuit1;
 
                     seriesSubcircuit1.Nodes.Add(parallelSubcircuit2);
+                    parallelSubcircuit2.Parent = seriesSubcircuit1;
                     parallelSubcircuit2.Nodes.Add(capacitor2);
+                    capacitor2.Parent = parallelSubcircuit2;
                     parallelSubcircuit2.Nodes.Add(seriesSubcircuit2);
+                    seriesSubcircuit2.Parent = parallelSubcircuit2;
                     seriesSubcircuit2.Nodes.Add(parallelSubcircuit3);
+                    parallelSubcircuit3.Parent = seriesSubcircuit2;
                     seriesSubcircuit2.Nodes.Add(resistor7);
+                    resistor7.Parent = seriesSubcircuit2;
                     parallelSubcircuit3.Nodes.Add(resistor5);
+                    resistor5.Parent = parallelSubcircuit3;
                     parallelSubcircuit3.Nodes.Add(inductor8);
+                    inductor8.Parent = parallelSubcircuit3;
                     parallelSubcircuit2.Nodes.Add(inductor6);
+                    inductor6.Parent = parallelSubcircuit2;
                     seriesSubcircuit1.Nodes.Add(resistor3);
+                    resistor3.Parent = seriesSubcircuit1;
 
                     circuit.AddAfter(null, seriesSubcircuit1);
                     break;
