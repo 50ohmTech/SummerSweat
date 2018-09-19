e1a025368d26665697add7100fab96ad68d02a65
Dmitriy Mekh <codemekh@gmail.com>
2018-09-19 22:49:09 +0700

    Conflict fix

---
+++ МЕХ/Circuit/ModelTests/CircuitTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace ModelTests
+{
+	internal class CircuitTests
+	{
+		#region Public methods
+
+		[Test(Description = "Тест правильности расчета сложной цепи")]
+		public void NodeTest()
+		{
+		}
+
+		#endregion
+	}
+}
