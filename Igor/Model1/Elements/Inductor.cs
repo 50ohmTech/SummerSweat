d08c6eef0320a06cbaab6aee12191f5a1a998a84
Igor Shagaev <igor.shagaev15@gmail.com>
2018-09-24 18:30:39 +0300

    Правки структуры проекта.

---
+++ Igor/Model1/Elements/Inductor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Numerics;
+
+namespace Model.Elements
+{
+    /// <summary>
+    ///     Индуктивность
+    /// </summary>
+    public class Inductor : ElementBase
+    {
+        #region Constructor
+
+        /// <summary>
+        ///     Конструктор для создание индуктивности.
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="inductance">Индуктивность</param>
+        public Inductor(string name, double inductance) : base(name, inductance)
+        {
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        ///     Расчет комплексного сопротивления индуктивности.
+        /// </summary>
+        /// <param name="frequency">Частоста</param>
+        /// <returns>Комплексное сопротивление</returns>
+        public override Complex CalculateZ(double frequency)
+        {
+            var valueZ = 2 * Math.PI * frequency * Value;
+            return new Complex(0, valueZ);
+        }
+
+        #endregion
+    }
+}
