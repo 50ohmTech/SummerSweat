3b9bd2ce022cdd90f1692f0c3b95ad3977f893ce
YulyaNovichkova <wwwyulya7@gmail.com>
2018-09-26 21:38:00 +0700

    Добавлена базовая реализация.

--- CircuitModel/Circuit.cs
+++ CircuitModel/Circuit.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Numerics;
 
 namespace CircuitModel
@@ -10,14 +7,47 @@ namespace CircuitModel
     /// <summary>
     /// Класс цепи.
     /// </summary>
-    class Circuit
+    public class Circuit
     {
+        #region ~ Приватные переменные ~
+
         /// <summary>
         /// Корень дерева.
         /// </summary>
         private INode _root;
 
+        #endregion
+
+        #region ~ Свойства ~
+
+        public INode Root { get; set; }
+
+        #endregion
+
         #region ~ Публичные методы ~
+
+        #region Недописанная функция, в последствии будет исправлено.
+        /// <summary>
+        /// Добавление нового узла.
+        /// </summary>
+        /// <param name="node"> Узел. </param>
+        /// <param name="newNode"> Новый узел. </param>
+        //public void AddAfter(INode node, INode newNode)
+        //{
+        //    if (IsEmpty())
+        //    {
+        //        Root = newNode;
+        //        return;
+        //    }
+
+        //    if (newNode == null)
+        //    {
+        //        throw new ArgumentNullException(nameof(newNode), 
+        //            "Выбранный узел пуст.");
+        //    }
+
+        //}
+        #endregion
 
         /// <summary>
         /// Расчет импеданса цепи для списка частот.
@@ -28,7 +58,8 @@ namespace CircuitModel
         {
             if (frequencies == null)
             {
-                throw new ArgumentNullException(nameof(frequencies));
+                throw new ArgumentNullException(
+                    "Значение частоты должно быть больше нуля!");
             }
 
             var impedances = new List<Complex>();
@@ -38,6 +69,43 @@ namespace CircuitModel
                 impedances.Add(_root.CalculateZ(frequency));
             }
             return impedances;
+        }
+
+        /// <summary>
+        /// Удаление электрической цепи.
+        /// </summary>
+        public void Clear()
+        {
+            Root = null;
+        }
+
+        /// <summary>
+        /// Проверка: присутствует ли элемент в цепи.
+        /// </summary>
+        /// <returns> Результат: да или нет. </returns>
+        public bool IsEmpty()
+        {
+            return Root == null;
+        }
+
+        /// <summary>
+        /// Удаление элемента.
+        /// </summary>
+        public void Remove(INode node)
+        {
+            if (node == Root)
+            {
+                Root = null;
+                return;
+            }
+
+            if (node == null)
+            {
+                throw new ArgumentNullException(nameof(node), 
+                    "Выбранный узел пустой.");
+            }
+
+            node.Parent.Nodes.Remove(node);
         }
 
         #endregion
